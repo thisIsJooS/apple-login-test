007c8f609aa1e3a65e687d6641cb238162a3ae37
Seungwoo, Joo <thisis.joos@gmail.com>
2023-08-26 18:59:44 +0900

    feat: apple login #3
    AppleIdTokenResponseDto @ToString for log check

--- src/main/java/demo/social/applelogin/AppleIdTokenResponseDto.java
+++ src/main/java/demo/social/applelogin/AppleIdTokenResponseDto.java
@@ -3,9 +3,11 @@ package demo.social.applelogin;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
+@ToString
 public class AppleIdTokenResponseDto {
         private String state;
         private String code;
